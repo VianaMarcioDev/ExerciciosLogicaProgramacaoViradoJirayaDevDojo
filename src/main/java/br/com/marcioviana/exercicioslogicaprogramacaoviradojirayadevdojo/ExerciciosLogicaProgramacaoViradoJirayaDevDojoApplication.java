8976081defed3b3516125adcfa88de96505b5b22
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2023-08-06 20:16:38 -0300

    Initial commit

---
+++ src/main/java/br/com/marcioviana/exercicioslogicaprogramacaoviradojirayadevdojo/ExerciciosLogicaProgramacaoViradoJirayaDevDojoApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcioviana.exercicioslogicaprogramacaoviradojirayadevdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosLogicaProgramacaoViradoJirayaDevDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosLogicaProgramacaoViradoJirayaDevDojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcioviana/exercicioslogicaprogramacaoviradojirayadevdojo/ExerciciosLogicaProgramacaoViradoJirayaDevDojoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcioviana.exercicioslogicaprogramacaoviradojirayadevdojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosLogicaProgramacaoViradoJirayaDevDojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
